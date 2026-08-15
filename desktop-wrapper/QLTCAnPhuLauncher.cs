144e9676b34f02b6d11ebca350ec87e6848827e9
lengochieu1211-sudo <314247999+lengochieu1211-sudo@users.noreply.github.com>
2026-08-15 21:08:13 +0700

    Refresh desktop launcher for latest zip build

--- desktop-wrapper/QLTCAnPhuLauncher.cs
+++ desktop-wrapper/QLTCAnPhuLauncher.cs
@@ -7,7 +7,7 @@ namespace QLTCAnPhu
 {
     internal static class Program
     {
-        private const string AppUrl = "https://com-example-qlct-61329.web.app/?app=desktop&v=20260815-2515c0d";
+        private const string AppUrl = "https://com-example-qlct-61329.web.app/?app=desktop&v=20260815-849e32b";
 
         [STAThread]
         private static void Main()
